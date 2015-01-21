58a161597947b17e7502ba49c743f633cb2641e4
gguerrero11 <gguerrero11@gmail.com>
2015-01-20 17:01:48 -0800

    Initial Commit

---
+++ AGoodStart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGoodStart
+//
+//  Created by Gabriel Guerrero on 1/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
